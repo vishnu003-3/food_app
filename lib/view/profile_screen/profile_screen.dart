af9e8f24a6fafc37e7e6a7b8c261770ca15c8ead
vishnu003-3 <vishnunarayanan3004@gmail.com>
2024-09-25 08:45:53 +0530

    food_app_profile

--- lib/view/profile_screen/profile_screen.dart
+++ lib/view/profile_screen/profile_screen.dart
@@ -27,133 +27,132 @@ class ProfileScreen extends StatelessWidget {
 
   Padding _buildDataSection() {
     return Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
             children: [
-              Column(
-                children: [
-                  Text(
-                    "Recipe",
-                    style: TextStyle(
-                        color: ColorConstants.lightGrey,
-                        fontSize: 12,
-                        fontWeight: FontWeight.normal),
-                  ),
-                  Text(
-                    "3",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
+              Text(
+                "Recipe",
+                style: TextStyle(
+                    color: ColorConstants.lightGrey,
+                    fontSize: 12,
+                    fontWeight: FontWeight.normal),
               ),
-              Column(
-                children: [
-                  Text(
-                    "Videos",
-                    style: TextStyle(
-                        color: ColorConstants.lightGrey,
-                        fontSize: 12,
-                        fontWeight: FontWeight.normal),
-                  ),
-                  Text(
-                    "13",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Text(
-                    "Followers",
-                    style: TextStyle(
-                        color: ColorConstants.lightGrey,
-                        fontSize: 12,
-                        fontWeight: FontWeight.normal),
-                  ),
-                  Text(
-                    "140k",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Text(
-                    "Following",
-                    style: TextStyle(
-                        color: ColorConstants.lightGrey,
-                        fontSize: 12,
-                        fontWeight: FontWeight.normal),
-                  ),
-                  Text(
-                    "120",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
+              Text(
+                "3",
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
             ],
           ),
-        );
+          Column(
+            children: [
+              Text(
+                "Videos",
+                style: TextStyle(
+                    color: ColorConstants.lightGrey,
+                    fontSize: 12,
+                    fontWeight: FontWeight.normal),
+              ),
+              Text(
+                "13",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Text(
+                "Followers",
+                style: TextStyle(
+                    color: ColorConstants.lightGrey,
+                    fontSize: 12,
+                    fontWeight: FontWeight.normal),
+              ),
+              Text(
+                "140k",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Text(
+                "Following",
+                style: TextStyle(
+                    color: ColorConstants.lightGrey,
+                    fontSize: 12,
+                    fontWeight: FontWeight.normal),
+              ),
+              Text(
+                "120",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
   Padding _buildProfileSection() {
     return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-          child: Column(children: [
-            Row(
-              children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage(ImageConstants.DP),
-                  radius: 50,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+      child: Column(children: [
+        Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage(ImageConstants.DP),
+              radius: 50,
+            ),
+            Spacer(),
+            Container(
+              height: 36,
+              width: 107,
+              decoration: BoxDecoration(
+                  border:
+                      Border.all(color: ColorConstants.PRIMARY_COLOR, width: 1),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                child: Text(
+                  "Edit profile",
+                  style: TextStyle(
+                      color: ColorConstants.PRIMARY_COLOR,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold),
                 ),
-                Spacer(),
-                Container(
-                  height: 36,
-                  width: 107,
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          color: ColorConstants.PRIMARY_COLOR, width: 1),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Center(
-                    child: Text(
-                      "Edit profile",
-                      style: TextStyle(
-                          color: ColorConstants.PRIMARY_COLOR,
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                )
-              ],
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Alessandra Blair",
-                style: TextStyle(
-                    color: ColorConstants.mainBlack,
-                    fontWeight: FontWeight.bold),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Hello world I’m Alessandra Blair, I’m \nfrom Italy 🇮🇹 I love cooking so much!",
-                style: TextStyle(
-                    color: ColorConstants.lightBlack,
-                    fontSize: 14,
-                    fontWeight: FontWeight.normal),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-          ]),
-        );
+            )
+          ],
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            "Alessandra Blair",
+            style: TextStyle(
+                color: ColorConstants.mainBlack, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            "Hello world I’m Alessandra Blair, I’m \nfrom Italy 🇮🇹 I love cooking so much!",
+            style: TextStyle(
+                color: ColorConstants.lightBlack,
+                fontSize: 14,
+                fontWeight: FontWeight.normal),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+      ]),
+    );
   }
 }
